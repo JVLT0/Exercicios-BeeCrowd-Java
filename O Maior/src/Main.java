c91798a65cf078ed27043a888aedc4582af010a5
Jão <joaovitorleal9999@gmail.com>
2024-08-17 22:36:02 -0300

    Exercicios

---
+++ O Maior/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        int X = input.nextInt();
+        double Y = input.nextDouble();
+
+        double media = X / Y;
+
+        System.out.printf("%.3f km/l\n",media);
+    }
+ 
+}
